547714b6bccb7f06b223c9555cbc72e2dd5fd308
Diego Aguirre <u202319891@upc.edu.pe>
2025-11-24 23:42:40 -0500

    Mejora del Menu

--- src/app/components/menu/menu.ts
+++ src/app/components/menu/menu.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatExpansionModule } from '@angular/material/expansion';
@@ -6,11 +7,21 @@ import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterLink, RouterModule } from '@angular/router';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
-  imports: [MatIconModule, MatButtonModule, MatToolbarModule, MatMenuModule, RouterLink, RouterModule, MatSidenavModule, MatListModule, MatExpansionModule],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    RouterLink,
+    RouterModule,
+    MatSidenavModule,
+    MatListModule,
+    MatExpansionModule,
+    RouterOutlet],
   templateUrl: './menu.html',
   styleUrl: './menu.css',
 })
