29c236eab1bf76f3d7440fedabfe64d1e4fa14b4
LuisCamus <u202319510@upc.edu.pe>
2025-11-26 18:21:25 -0500

    Seguridad

---
+++ src/app/guard/seguridad-guard.ts
@@ -0,0 +1,28 @@
+import { CanActivateFn, Router } from '@angular/router';
+
+// 1. Importar MatSnackBar
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { inject } from '@angular/core';
+
+export const seguridadGuard: CanActivateFn = (route, state) => {
+  const lService = inject();
+  const router = inject(Router);
+  // 2. Inyectar el servicio de notificaciones
+  const snackBar = inject(MatSnackBar);
+
+  const rpta = lService.verificar();
+
+  if (!rpta) {
+    // 3. Mostrar el mensaje
+    snackBar.open('Debe Iniciar Sesión para acceder a esta página', 'Aceptar', {
+      duration: 3000, // El mensaje dura 3 segundos
+      horizontalPosition: 'center', // Alineación horizontal
+      verticalPosition: 'top', // Aparece arriba (más visible)
+    });
+
+    router.navigate(['/login']);
+    return false;
+  }
+
+  return rpta;
+};
