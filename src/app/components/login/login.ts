2e2b3e4dc0131381ba27114ac917e9114a687380
Diego Aguirre <u202319891@upc.edu.pe>
2025-11-26 14:00:50 -0500

    seguridad Login

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,59 +1,62 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { Router, RouterLink } from '@angular/router';
+import { Loginservice } from '../../services/loginservice';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { JwtRequestDTO } from '../../models/jwtRequestDTO';
+import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-login',
   imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    MatFormFieldModule,
-    MatInputModule,
+    MatFormFieldModule, 
+    FormsModule,
+    MatInputModule, 
     MatButtonModule,
     MatCardModule,
+    CommonModule,
     MatIconModule,
-    RouterLink,
+    RouterLink
   ],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {
-  form: FormGroup;
+export class Login implements OnInit {
+  constructor(
+    private loginService: Loginservice,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) {}
+
+  username: string = "";
+  password: string = "";
+  mensaje: string = "";
   hidePassword = true;
 
-  constructor(private formBuilder: FormBuilder, private router: Router) {
-    // Inicializamos el formulario con validaciones básicas
-    this.form = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$')]], // Validación de email estándar
-      password: ['', Validators.required],
-    });
+  ngOnInit(): void {
+      
   }
 
-  // Método que se ejecuta al enviar el formulario
-  ingresar(): void {
-    if (this.form.valid) {
-      console.log('Datos del login:', this.form.value);
-      // Aquí iría la lógica de conexión al backend.
-      // Por ahora, solo redirigimos al menú para simular el éxito.
-      this.router.navigate(['/menu']);
-    } else {
-      // Si el formulario no es válido, marcamos los campos para mostrar errores
-      this.form.markAllAsTouched();
-    }
-  }
+  login() {
+        let request = new JwtRequestDTO();
+        request.username = this.username
+        request.password = this.password
 
-  // --- Getters para facilitar el acceso en el HTML ---
-  get email() {
-    return this.form.get('email');
-  }
+        this.loginService.login(request).subscribe((data: any) => {
+          sessionStorage.setItem('token', data.jwttoken)
+          this.router.navigate(['menu'])
+        },
+        (error) => {
+          this.mensaje = 'Credenciales incorrectas, intente de nuevo.'
+          this.snackBar.open(this.mensaje, 'Cerrar', {duration: 3000})
+        }
+      )
+      }
 
-  get password() {
-    return this.form.get('password');
-  }
 }
