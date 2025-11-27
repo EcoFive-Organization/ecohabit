b3bf3ed6723327ac154041b2c83c61683599787b
LuisCamus <u202319510@upc.edu.pe>
2025-11-27 07:18:59 -0500

    Correcion login conexion

--- src/app/services/loginservice.ts
+++ src/app/services/loginservice.ts
@@ -2,16 +2,19 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { JwtRequestDTO } from '../models/jwtRequestDTO';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { environment } from '../../environments/environment';
+
+const base_url = environment.base
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class Loginservice {
-
+  private url = `${base_url}/login`;
   constructor(private http: HttpClient) {}
 
   login(request: JwtRequestDTO) {
-    return this.http.post('http://localhost:8080/login', request)
+    return this.http.post(this.url, request);
   }
 
   verificar() {
@@ -29,5 +32,4 @@ export class Loginservice {
     const decodedToken = helper.decodeToken(token);
     return decodedToken?.role;
   }
-
 }
