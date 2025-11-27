d7ccac87c78689bc3c352d929c8922c2d80c2b6b
LuisCamus <u202319510@upc.edu.pe>
2025-11-27 06:57:10 -0500

    Implementación de credenciales para conexión con backend en la nube

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  base: 'http://localhost:8080',
+  base: 'https://ecohabitproject.onrender.com',
   paypalClientId:
     'ARGhbols4-H3OSEelx9kL9Of8LpCB_JCvU2vxnQjO3avsPFR3d8WmyITT85vc8mjwlhBUIyYrdaCFgZO',
 };
