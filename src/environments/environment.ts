1189946175f8cdec7b2470572fe7604dd6155e03
LuisCamus <u202319510@upc.edu.pe>
2025-11-29 22:05:43 -0500

    Tryhardeada de ultimo minuto

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  //base: 'http://localhost:8080',
-  base: 'https://ecohabitproject.onrender.com',
+  base: 'http://localhost:8080',
+  //base: 'https://ecohabitproject.onrender.com',
   paypalClientId:
     'ARGhbols4-H3OSEelx9kL9Of8LpCB_JCvU2vxnQjO3avsPFR3d8WmyITT85vc8mjwlhBUIyYrdaCFgZO',
 };
